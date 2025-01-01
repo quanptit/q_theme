1041a847a9f0143513e16f71a32492c0237bc537
quanptit <quanptit410@gmail.com>
2025-01-01 12:45:59 +0700

    update

---
+++ lib/src/view_models/theme_viewmodel.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'theme_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$themeViewModelHash() => r'ea17461b7bc05c44a0926ece1e0700b3aa16e8a3';
+
+/// See also [ThemeViewModel].
+@ProviderFor(ThemeViewModel)
+final themeViewModelProvider =
+    NotifierProvider<ThemeViewModel, ThemeMode>.internal(
+  ThemeViewModel.new,
+  name: r'themeViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$themeViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ThemeViewModel = Notifier<ThemeMode>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
